1f78b02e01bf2b2ee22b472698183a407524c2ec
TTPBoss <64545935+TTPBoss@users.noreply.github.com>
2024-04-20 23:25:03 -0400

    setup

---
+++ scholarship_app/server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('Hello World');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+});
+
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.post('/submit-application', (req, res) => {
+    const { name, email, financial_info } = req.body;
+    console.log('Application Received:', name, email, financial_info);
+    res.send('Application submitted successfully!');
+});
