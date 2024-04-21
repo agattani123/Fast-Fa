fbb4b7b7a38495f4a32d1e03cf456e7430ceabe5
TTPBoss <64545935+TTPBoss@users.noreply.github.com>
2024-04-20 23:38:51 -0400

    Sucsessful submit type beat

--- scholarship_app/server.js
+++ scholarship_app/server.js
@@ -3,6 +3,7 @@ const app = express();
 const PORT = 3000;
 
 app.use(express.json());
+app.use(express.static('public')); // Serve static files from the 'public' directory
 
 app.get('/', (req, res) => {
     res.send('Hello World');
