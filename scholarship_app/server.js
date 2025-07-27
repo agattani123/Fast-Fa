aaa7a0fe6f737c97042014a5734032ff26141795
Arnav Gattani <112846923+agattani123@users.noreply.github.com>
2025-07-26 17:43:48 -0700

    Update server.js

--- scholarship_app/server.js
+++ scholarship_app/server.js
@@ -56,14 +56,14 @@ app.post('/submit-application', async (req, res) => {
     const output = await generateText(`"
 
     Making this prompt super simplistic. 
-    // Take a prompt and give me a list and URLs to specific scholarships that would specifically give me access to opportunities." + 
-    // "I do not want any opportunity that are generalized to a big group of students and rather want opportunities as specific to the prompt as possible." +
-    // "Each opportunity should include a description of what that student specifically a good fit.  Facets include age, race, gender, location, future occupation, interests, and future plans." + 
-    // "Also, the complete list should have addressed at least each facet provided in the prompt. For example, if the student wants to study in Hawaii then at least one opportunity in that area and please write that out explicitly." +
-    // "If a student is from a certain area, you must provide at least one opportunity that is specific to that area and please write this our explicitly." +
-    // "Please be VERY specific as to which part of the prompt this opportunity addresses. If it is not in the prompt, absolutely do not include that opportunity. Only include opportunities DIRECTLY applicable to the prompt." +
-    // "It must be very specific to the prompt and tailored directly to it. Give me at least 10 distinct results. Structure the output as follows: I want an HTML list with each opportunity having the fields" +
-    // "\"Scholarship\" (Opportunity name), \"Why me?\" (Description of why I am a good fit and what Facets it addresses), \"How much?\" (Prize Amount), and \"Deadline\" (around when should you apply by), and \"APPLY\" (a URL of where to apply, have the link hyperlinked to APPLY and not in front of a colon). I just want the html do not include any other text before and after (include any quotation marks, i just want the html)" +
+    Take a prompt and give me a list and URLs to specific scholarships that would specifically give me access to opportunities." + 
+    "I do not want any opportunity that are generalized to a big group of students and rather want opportunities as specific to the prompt as possible." +
+    "Each opportunity should include a description of what that student specifically a good fit.  Facets include age, race, gender, location, future occupation, interests, and future plans." + 
+    "Also, the complete list should have addressed at least each facet provided in the prompt. For example, if the student wants to study in Hawaii then at least one opportunity in that area and please write that out explicitly." +
+    "If a student is from a certain area, you must provide at least one opportunity that is specific to that area and please write this our explicitly." +
+    "Please be VERY specific as to which part of the prompt this opportunity addresses. If it is not in the prompt, absolutely do not include that opportunity. Only include opportunities DIRECTLY applicable to the prompt." +
+    "It must be very specific to the prompt and tailored directly to it. Give me at least 10 distinct results. Structure the output as follows: I want an HTML list with each opportunity having the fields" +
+    "\"Scholarship\" (Opportunity name), \"Why me?\" (Description of why I am a good fit and what Facets it addresses), \"How much?\" (Prize Amount), and \"Deadline\" (around when should you apply by), and \"APPLY\" (a URL of where to apply, have the link hyperlinked to APPLY and not in front of a colon). I just want the html do not include any other text before and after (include any quotation marks, i just want the html)" +
     
     
     "The prompt is follows: ${financial_info}`);
